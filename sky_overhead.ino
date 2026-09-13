c9a16eeef367356a178b41093f99249f4ca2e88e
Marcell Kovács <me@marcellkovacs.com>
2026-09-13 14:58:37 +0200

    Enlarge and align aircraft QR

--- sky_overhead.ino
+++ sky_overhead.ino
@@ -123,13 +123,13 @@ namespace ui {
   constexpr int FOOTER_Y = 452;
 
   // Live-aircraft QR in the top-left corner.
-  constexpr int QR_VERSION = 9;
+  constexpr int QR_VERSION = 13;
   constexpr int QR_QUIET_ZONE = 4;
   constexpr int QR_MODULES = 4 * QR_VERSION + 17;
   constexpr int QR_BUFFER_SIZE = (QR_MODULES * QR_MODULES + 7) / 8;
   constexpr int QR_SIZE = QR_MODULES + 2 * QR_QUIET_ZONE;
-  constexpr int QR_X = MARGIN;
-  constexpr int QR_Y = MARGIN;
+  constexpr int QR_X = MARGIN / 2;
+  constexpr int QR_Y = MARGIN / 2;
 
 }
 
